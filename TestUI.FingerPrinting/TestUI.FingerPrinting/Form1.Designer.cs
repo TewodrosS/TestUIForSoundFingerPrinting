0e34541dbefd8e2fec846c4c857bd651d292f3fb
tedydag@hotmail.com <tedydag@hotmail.com>
2015-08-16 16:48:41 -0500

    initial code for UI

---
+++ TestUI.FingerPrinting/TestUI.FingerPrinting/Form1.Designer.cs
@@ -0,0 +1,206 @@
+﻿namespace TestUI.FingerPrinting
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this._tbSingleFile = new System.Windows.Forms.TextBox();
+            this.BtnBrowse = new System.Windows.Forms.Button();
+            this._btnStart = new System.Windows.Forms.Button();
+            this._tbRootFolder = new System.Windows.Forms.TextBox();
+            this._btnLoad = new System.Windows.Forms.Button();
+            this._dgvFillDatabase = new System.Windows.Forms.DataGridView();
+            this.Artist = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Title = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ISRC = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Length = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.SubFingerprints = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this._dgvFillDatabase)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // _tbSingleFile
+            // 
+            this._tbSingleFile.Location = new System.Drawing.Point(40, 60);
+            this._tbSingleFile.Name = "_tbSingleFile";
+            this._tbSingleFile.Size = new System.Drawing.Size(657, 31);
+            this._tbSingleFile.TabIndex = 0;
+            // 
+            // BtnBrowse
+            // 
+            this.BtnBrowse.Location = new System.Drawing.Point(40, 111);
+            this.BtnBrowse.Name = "BtnBrowse";
+            this.BtnBrowse.Size = new System.Drawing.Size(156, 45);
+            this.BtnBrowse.TabIndex = 1;
+            this.BtnBrowse.Text = "Browse";
+            this.BtnBrowse.UseVisualStyleBackColor = true;
+            this.BtnBrowse.Click += new System.EventHandler(this.BtnBrowse_Click);
+            // 
+            // _btnStart
+            // 
+            this._btnStart.Location = new System.Drawing.Point(263, 111);
+            this._btnStart.Name = "_btnStart";
+            this._btnStart.Size = new System.Drawing.Size(156, 45);
+            this._btnStart.TabIndex = 2;
+            this._btnStart.Text = "Start";
+            this._btnStart.UseVisualStyleBackColor = true;
+            this._btnStart.Click += new System.EventHandler(this.BtnStartClick);
+            // 
+            // _tbRootFolder
+            // 
+            this._tbRootFolder.Location = new System.Drawing.Point(40, 284);
+            this._tbRootFolder.Name = "_tbRootFolder";
+            this._tbRootFolder.Size = new System.Drawing.Size(657, 31);
+            this._tbRootFolder.TabIndex = 88;
+            this._tbRootFolder.TextChanged += new System.EventHandler(this._tbRootFolder_TextChanged);
+            this._tbRootFolder.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.TbRootFolderMouseDoubleClick);
+            // 
+            // _btnLoad
+            // 
+            this._btnLoad.Location = new System.Drawing.Point(40, 337);
+            this._btnLoad.Name = "_btnLoad";
+            this._btnLoad.Size = new System.Drawing.Size(167, 74);
+            this._btnLoad.TabIndex = 89;
+            this._btnLoad.Text = "Load";
+            this._btnLoad.UseVisualStyleBackColor = true;
+            this._btnLoad.Click += new System.EventHandler(this._btnLoad_Click);
+            // 
+            // _dgvFillDatabase
+            // 
+            this._dgvFillDatabase.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this._dgvFillDatabase.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this._dgvFillDatabase.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Artist,
+            this.Title,
+            this.ISRC,
+            this.Length,
+            this.SubFingerprints});
+            this._dgvFillDatabase.Location = new System.Drawing.Point(40, 420);
+            this._dgvFillDatabase.Margin = new System.Windows.Forms.Padding(6);
+            this._dgvFillDatabase.Name = "_dgvFillDatabase";
+            this._dgvFillDatabase.ReadOnly = true;
+            this._dgvFillDatabase.Size = new System.Drawing.Size(1384, 355);
+            this._dgvFillDatabase.TabIndex = 90;
+            // 
+            // Artist
+            // 
+            this.Artist.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.Artist.HeaderText = "Artist";
+            this.Artist.Name = "Artist";
+            this.Artist.ReadOnly = true;
+            // 
+            // Title
+            // 
+            this.Title.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.Title.FillWeight = 200F;
+            this.Title.HeaderText = "Title";
+            this.Title.Name = "Title";
+            this.Title.ReadOnly = true;
+            // 
+            // ISRC
+            // 
+            this.ISRC.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.ISRC.HeaderText = "ISRC";
+            this.ISRC.Name = "ISRC";
+            this.ISRC.ReadOnly = true;
+            // 
+            // Length
+            // 
+            this.Length.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.Length.FillWeight = 50F;
+            this.Length.HeaderText = "Length";
+            this.Length.Name = "Length";
+            this.Length.ReadOnly = true;
+            // 
+            // SubFingerprints
+            // 
+            this.SubFingerprints.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.SubFingerprints.FillWeight = 50F;
+            this.SubFingerprints.HeaderText = "SubFingerprints";
+            this.SubFingerprints.Name = "SubFingerprints";
+            this.SubFingerprints.ReadOnly = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(40, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(322, 25);
+            this.label1.TabIndex = 91;
+            this.label1.Text = "Browse a file you want to search";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(35, 244);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(310, 25);
+            this.label2.TabIndex = 92;
+            this.label2.Text = "Doble click to load music folder";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1477, 984);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this._dgvFillDatabase);
+            this.Controls.Add(this._btnLoad);
+            this.Controls.Add(this._tbRootFolder);
+            this.Controls.Add(this._btnStart);
+            this.Controls.Add(this.BtnBrowse);
+            this.Controls.Add(this._tbSingleFile);
+            this.Name = "Form1";
+            this.Text = "SoundFingerprint";
+            ((System.ComponentModel.ISupportInitialize)(this._dgvFillDatabase)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox _tbSingleFile;
+        private System.Windows.Forms.Button BtnBrowse;
+        private System.Windows.Forms.Button _btnStart;
+        private System.Windows.Forms.TextBox _tbRootFolder;
+        private System.Windows.Forms.Button _btnLoad;
+        private System.Windows.Forms.DataGridView _dgvFillDatabase;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Artist;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Title;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ISRC;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Length;
+        private System.Windows.Forms.DataGridViewTextBoxColumn SubFingerprints;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ TestUI.FingerPrinting/TestUI.FingerPrinting/Form1.cs
@@ -0,0 +1,405 @@
+﻿using SoundFingerprinting;
+using SoundFingerprinting.Audio;
+using SoundFingerprinting.Audio.Bass;
+using SoundFingerprinting.Audio.NAudio;
+using SoundFingerprinting.Builder;
+using SoundFingerprinting.Configuration;
+using SoundFingerprinting.DAO;
+using SoundFingerprinting.DAO.Data;
+using SoundFingerprinting.SQL;
+using SoundFingerprinting.Strides;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Security.Permissions;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using TestUI.FingerPrinting.Properties;
+
+namespace TestUI.FingerPrinting
+{
+    public partial class Form1 : Form
+    {
+        private readonly List<string> filters = new List<string>(new[] { "*.mp3", "*.wav", "*.ogg", "*.flac" });
+        private List<string> fileList = new List<string>();
+        private HashAlgorithm hashAlgorithm = HashAlgorithm.LSH;
+        private const int MaxThreadToProcessFiles = 4; /*2 MaxThreadToProcessFiles used to process the files*/
+        private const int MinTrackLength = 20; /*20 sec - minimal track length*/
+        private const int MaxTrackLength = 60 * 10; /*15 min - maximal track length*/
+        private IQueryCommandBuilder queryCommandBuilder = new QueryCommandBuilder();
+        private ITagService tagService = new BassTagService();
+        private IModelService modelService = new SqlModelService();
+        private IAudioService audioService = new NAudioService();
+        IFingerprintCommandBuilder fingerprintCommandBuilder = new FingerprintCommandBuilder();
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            Icon = Resources.Sound;
+            
+            
+            string[] items = Enum.GetNames(typeof(StrideType));
+
+                      
+        }
+
+        private void BtnBrowse_Click(object sender, EventArgs e)
+        {
+            string filter = WinUtils.GetMultipleFilter("Audio files", filters);
+            OpenFileDialog ofd = new OpenFileDialog { Filter = filter, Multiselect = true };
+
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                _tbSingleFile.Text = null;
+                foreach (string file in ofd.FileNames)
+                {
+                    _tbSingleFile.Text += "\"" + Path.GetFileName(file) + "\" ";
+                }
+
+                foreach (string file in ofd.FileNames.Where(file => !fileList.Contains(file)))
+                {
+                    _btnStart.Enabled = true;
+                    fileList.Add(file);
+                }                
+            }
+        }
+
+        private void BtnStartClick(object sender, EventArgs e)
+        {
+            DefaultFingerprintConfiguration configuration = new DefaultFingerprintConfiguration();
+            switch (hashAlgorithm)
+            {
+                case HashAlgorithm.LSH:
+                    if (!fileList.Any())
+                    {
+                        MessageBox.Show(Resources.SelectFolderWithSongs, Resources.Songs, MessageBoxButtons.OK, MessageBoxIcon.Information);
+                        break;
+                    }
+
+                    WinQueryResults winQueryResults = new WinQueryResults(10, 20, 25, 4, 5,    
+                        WinUtils.GetStride(StrideType.IncrementalRandom, 512, 256, configuration.SamplesPerFingerprint),
+                        tagService,
+                        modelService,
+                        audioService,
+                        queryCommandBuilder);
+
+                    winQueryResults.Show();
+                    winQueryResults.Refresh();
+                    winQueryResults.ExtractCandidatesWithMinHashAlgorithm(fileList);
+                    break;
+                case HashAlgorithm.NeuralHasher:
+                    throw new NotImplementedException();
+                case HashAlgorithm.None:
+                    throw new NotImplementedException();
+            }
+        }
+
+        [FileDialogPermission(SecurityAction.Demand)]
+        private void TbRootFolderMouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            FolderBrowserDialog fbd = new FolderBrowserDialog();
+            if (fbd.ShowDialog() == DialogResult.OK)
+            {
+                _tbRootFolder.Text = fbd.SelectedPath;
+                Cursor = Cursors.WaitCursor;
+                _tbRootFolder.Enabled = false;
+                fileList = WinUtils.GetFiles(filters, _tbRootFolder.Text);
+                Invoke(new Action(RestoreCursorShowTotalFilesCount));
+            }
+        }
+
+        private void _tbRootFolder_TextChanged(object sender, EventArgs e)
+        {                       
+            Cursor = Cursors.WaitCursor;
+            _tbRootFolder.Enabled = false;
+            fileList = WinUtils.GetFiles(filters, _tbRootFolder.Text);
+            Invoke(new Action(RestoreCursorShowTotalFilesCount));
+        }
+
+        private void RestoreCursorShowTotalFilesCount()
+        {
+            Cursor = Cursors.Default;
+            _tbRootFolder.Enabled = true;
+            if (fileList != null)
+            {               
+                _btnStart.Enabled = true;
+            }
+
+            _tbSingleFile.Text = null;
+        }
+
+        private void RootFolderIsSelected(object sender, EventArgs e)
+        {
+            Cursor = Cursors.WaitCursor;
+            _tbRootFolder.Enabled = false;
+            fileList = WinUtils.GetFiles(filters, _tbRootFolder.Text);
+            Invoke(new Action(RestoreCursorShowTotalFilesCount));
+        }
+
+        private void TbSingleFileTextChanged(object sender, EventArgs e)
+        {
+            if (File.Exists(_tbSingleFile.Text))
+            {
+                if (filters.Any(filter => filter.Contains(Path.GetExtension(_tbSingleFile.Text))))
+                {
+                    if (!fileList.Contains(_tbSingleFile.Text))
+                    {
+                        fileList.Add(_tbSingleFile.Text);
+                        _btnStart.Enabled = true;
+                    }
+
+                }
+            }
+        }
+
+        private bool IsDuplicateFile(string isrc, string artist, string title)
+        {
+            if (!string.IsNullOrEmpty(isrc))
+            {
+                return modelService.ReadTrackByISRC(isrc) != null;
+            }
+
+            return modelService.ReadTrackByArtistAndTitleName(artist, title).Any();
+        }
+
+        private void InsertInDatabase(int start, int end)
+        {
+            int topWavelets = 200;// (int)_nudTopWav.Value;
+            IStride stride = null;
+            Invoke(
+                new Action(
+                    () =>
+                    {
+                        stride = WinUtils.GetStride(
+                            StrideType.IncrementalRandom,
+                            5115,//(int)_nudStride.Value,
+                            0,
+                            new DefaultFingerprintConfiguration().SamplesPerFingerprint);
+                    }),
+                null);
+
+            //Action actionInterface = () =>
+            //{
+            //    _pbTotalSongs.PerformStep();
+            //    _nudProcessed.Value = processed;
+            //    _nudLeft.Value = left;
+            //    _nudBadFiles.Value = badFiles;
+            //    _nudDetectedDuplicates.Value = duplicates;
+            //};
+
+            Action<object[], Color> actionAddItems = (parameters, color) =>
+            {
+                int index = _dgvFillDatabase.Rows.Add(parameters);
+                _dgvFillDatabase.FirstDisplayedScrollingRowIndex = index;
+                if (color != Color.Empty)
+                {
+                    _dgvFillDatabase.Rows[index].DefaultCellStyle.BackColor = color;
+                }
+            };
+
+            for (int i = start; i < end; i++)
+            {
+                // Process the corresponding files
+                //if (stopFlag)
+                //{
+                //    return;
+                //}
+
+                TagInfo tags = tagService.GetTagInfo(fileList[i]); // Get Tags from file
+                if (tags == null || tags.IsEmpty)
+                {
+                    //badFiles++;
+                    //processed++;
+                    //left--;
+                    Invoke(actionAddItems, new object[] { "TAGS ARE NULL", fileList[i], 0, 0 }, Color.Red);
+                    //Invoke(actionInterface);
+                    continue;
+                }
+
+                string isrc = tags.ISRC;
+                string artist = tags.Artist; // Artist
+                string title = tags.Title; // Title
+                int releaseYear = tags.Year;
+                string album = tags.Album;
+                double duration = tags.Duration; // Duration
+
+                // Check whether the duration is OK
+                if (duration < MinTrackLength || duration > MaxTrackLength)
+                {
+                    // Duration too small
+                    //badFiles++;
+                    //processed++;
+                    //left--;
+                    Invoke(actionAddItems, new object[] { "Bad duration", fileList[i], 0, 0 }, Color.Red);
+                    //Invoke(actionInterface);
+                    continue;
+                }
+
+                // Check whether the tags are properly defined
+                if (string.IsNullOrEmpty(isrc) && (string.IsNullOrEmpty(artist) || string.IsNullOrEmpty(title)))
+                {
+                    //badFiles++;
+                    //processed++;
+                    //left--;
+                    Invoke(
+                        actionAddItems,
+                        new object[] { "ISRC Tag is missing. Skipping file...", fileList[i], 0, 0 },
+                        Color.Red);
+                    //Invoke(actionInterface);
+                    continue;
+                }
+
+                IModelReference trackReference;
+                try
+                {
+                    lock (this)
+                    {
+                        // Check if this file is already in the database
+                        if (IsDuplicateFile(isrc, artist, title))
+                        {
+                            //duplicates++; // There is such file in the database
+                            //processed++;
+                            //left--;
+                            //Invoke(actionInterface);
+                            continue;
+                        }
+
+                        var track = new TrackData(isrc, artist, title, album, releaseYear, (int)duration);
+                        trackReference = modelService.InsertTrack(track); // Insert new Track in the database
+                    }
+                }
+                catch (Exception e)
+                {
+                    // catch any exception and abort the insertion
+                    //processed++;
+                    //left--;
+                    //badFiles++;
+                    MessageBox.Show(e.Message, Resources.Error, MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    //Invoke(actionInterface);
+                    return;
+                }
+
+                int count;
+                try
+                {
+                    var hashDatas = fingerprintCommandBuilder
+                                        .BuildFingerprintCommand()
+                                        .From(fileList[i])
+                                        .WithFingerprintConfig(
+                                            config =>
+                                            {
+                                                config.TopWavelets = topWavelets;
+                                                config.SpectrogramConfig.Stride = stride;
+                                            })
+                                        .UsingServices(audioService)
+                                        .Hash()
+                                        .Result; // Create SubFingerprints
+
+                    modelService.InsertHashDataForTrack(hashDatas, trackReference);
+                    count = hashDatas.Count;
+                }
+                catch (Exception e)
+                {
+                    // catch any exception and abort the insertion
+                    MessageBox.Show(e.Message, Resources.Error, MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    //badFiles++;
+                    //processed++;
+                    //left--;
+                    //Invoke(actionInterface);
+                    return;
+                }
+
+                Invoke(actionAddItems, new object[] { artist, title, isrc, duration, count }, Color.Empty);
+                //left--;
+                //processed++;
+                //Invoke(actionInterface);
+            }
+        }
+
+
+        private void _btnLoad_Click(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrEmpty(_tbRootFolder.Text) || !string.IsNullOrEmpty(_tbSingleFile.Text) && fileList == null)
+            {
+                fileList = new List<string>();
+                if (!string.IsNullOrEmpty(_tbRootFolder.Text))
+                {
+                    RootFolderIsSelected(this, null);
+                }
+
+                if (!string.IsNullOrEmpty(_tbSingleFile.Text))
+                {
+                    TbSingleFileTextChanged(this, null);
+                }
+            }
+
+            if (!fileList.Any())
+            {
+                MessageBox.Show(Resources.FileListEmpty, Resources.FileListEmptyCaption, MessageBoxButtons.OK, MessageBoxIcon.Information);
+                return;
+            }
+
+            //FadeAllControls(true); // Fade all controls
+
+            int rest = fileList.Count % MaxThreadToProcessFiles;
+            int filesPerThread = fileList.Count / MaxThreadToProcessFiles;
+
+            switch (hashAlgorithm)
+            {
+                case HashAlgorithm.LSH:
+                    break;
+                case HashAlgorithm.NeuralHasher:
+                    throw new NotImplementedException();
+                case HashAlgorithm.None:
+                    break;
+            }
+
+            BeginInvoke(new Action(() => { }), null);
+
+            //ResetControls();
+            int runningThreads = MaxThreadToProcessFiles;
+            for (int i = 0; i < MaxThreadToProcessFiles; i++)
+            {
+                // Start asynchronous operation
+                int start = i * filesPerThread; // Define start and end indexes
+                int end = (i == MaxThreadToProcessFiles - 1) ? i * filesPerThread + filesPerThread + rest : i * filesPerThread + filesPerThread;
+                Action<int, int> action = InsertInDatabase;
+                action.BeginInvoke(
+                    start,
+                    end,
+                    result =>
+                        {
+                            // End Asynchronous operation
+                            Action<int, int> item = (Action<int, int>)result.AsyncState;
+                            item.EndInvoke(result);
+                            Interlocked.Decrement(ref runningThreads);
+                            if (runningThreads == 0)
+                            {
+                                /********* END OF INSERTION PROCESS HERE!********/
+
+                                Invoke(
+                                    new Action(
+                                        () =>
+                                            {
+                                                //_pbTotalSongs.Visible = false;
+                                                //FadeAllControls(false);
+                                                _tbRootFolder.Text = null;
+                                                _tbSingleFile.Text = null;
+                                            }));
+                                MessageBox.Show(Resources.InsertionEnded, Resources.End, MessageBoxButtons.OK, MessageBoxIcon.Information);
+                            }
+                        },
+                    action);
+            }
+        
+        }
+
+    }
+}
